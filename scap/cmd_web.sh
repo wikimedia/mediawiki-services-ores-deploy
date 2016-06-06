ae71d842dfc0958e06922062dd09d49243332a6a
amir <Ladsgroup@gmail.com>
2016-06-06 23:19:26 +0000

    Update ORES submodule, fix restart
    Change-Id: I04c129acee328a94ce6ba1660de4182d5e19994a

--- scap/cmd_web.sh
+++ scap/cmd_web.sh
@@ -4,4 +4,3 @@ mkdir -p $venv
 virtualenv --python python3 --system-site-packages $venv
 $venv/bin/pip freeze | xargs $venv/bin/pip uninstall -y
 $venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/*.whl
-sudo service uwsgi-ores restart
