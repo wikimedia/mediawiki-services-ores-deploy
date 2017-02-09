030c269b6bba0b5cae4ae92755fbcb0fe4e63df5
halfak <aaron.halfaker@gmail.com>
2017-02-09 10:36:59 -0600

    Adds submodule sync to cmd_worker.sh
    Change-Id: I63c5ab8b72a8c65e504b545dbbe0706bdf642c52

--- scap/cmd_worker.sh
+++ scap/cmd_worker.sh
@@ -1,5 +1,8 @@
 venv="/srv/deployment/ores/venv"
 deploy_dir="/srv/deployment/ores/deploy"
+cd $deploy_dir
+git submodule sync
+git submodule update --init
 mkdir -p $venv
 virtualenv --python python3 --system-site-packages $venv
 $venv/bin/pip freeze | xargs $venv/bin/pip uninstall -y
