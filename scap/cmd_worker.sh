437fcf55452db1d435e5f5e425853a8849a0b00d
Adam Roses Wight <awight@wikimedia.org>
2017-10-31 12:31:25 -0500

    Remove the virtualenv directory each time
    Since we remove all the wheels, this doesn't add much overhead.  I was
    having some trouble with downgrading pip, so this change should make
    it safer to roll back across revscoring 2 -> 1.
    
    Bug: T179095
    Bug: T175180
    Change-Id: Ic7a2029bdec3d90a747d28920ffa2d62c4520d73

--- scap/cmd_worker.sh
+++ scap/cmd_worker.sh
@@ -3,9 +3,9 @@ deploy_dir="/srv/deployment/ores/deploy"
 cd $deploy_dir
 git submodule sync
 git submodule update --init
+rm -rf $venv
 mkdir -p $venv
 virtualenv --python python3 --system-site-packages $venv
-$venv/bin/pip freeze | xargs $venv/bin/pip uninstall -y
 [ -f $deploy_dir/submodules/wheels/pip-*.whl ] && $venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/pip-*.whl
 $venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/*.whl
 sudo service celery-ores-worker restart
