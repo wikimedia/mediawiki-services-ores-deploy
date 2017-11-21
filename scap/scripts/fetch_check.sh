7ab18f824b6e89facc37e156da72d2bfde19eec7
Adam Roses Wight <awight@wikimedia.org>
2017-11-21 12:19:56 -0500

    Build venv into deployed source dir
    This moves the python library installation to the fetch stage, and stores
    inside the source dir so we rollback faster.
    
    Bug: T181071
    Change-Id: Ibe44c2c3d7ec526a118a2b09a707616dd6cb3c05

--- scap/scripts/fetch_check.sh
+++ scap/scripts/fetch_check.sh
@@ -1,11 +1,16 @@
-venv="/srv/deployment/ores/venv"
-deploy_dir="/srv/deployment/ores/deploy"
+#!/bin/bash
+
+# Point at deployed dirs.
+deploy_dir="${SCAP_REV_PATH}"
+venv="${deploy_dir}/venv"
+
+# Pull submodules.
 cd $deploy_dir
 git submodule sync
 git submodule update --init
-rm -rf $venv
+
+# Install python libs.
 mkdir -p $venv
 virtualenv --python python3 --system-site-packages $venv
 [ -f $deploy_dir/submodules/wheels/pip-*.whl ] && $venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/pip-*.whl
 $venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/*.whl
-sudo service celery-ores-worker restart
