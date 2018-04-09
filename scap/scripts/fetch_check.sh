543901a78df32c4a9f269334919909b0479a223f
Adam Wight <awight@wikimedia.org>
2018-04-09 12:14:25 -0700

    Remove transitional virtualenv copy
    Bug: T181071
    Change-Id: I2232da649bb0b0a7d50f92f0f84016b99b9c334e

--- scap/scripts/fetch_check.sh
+++ scap/scripts/fetch_check.sh
@@ -3,7 +3,6 @@
 # Point at deployed dirs.
 deploy_dir="${SCAP_REV_PATH}"
 venv="${deploy_dir}/venv"
-venv_old=/srv/deployment/ores/venv
 
 # Pull submodules.
 cd $deploy_dir
@@ -15,9 +14,3 @@ mkdir -p $venv
 virtualenv --python python3 --system-site-packages --never-download $venv
 [ -f $deploy_dir/submodules/wheels/pip-*.whl ] && $venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/pip-*.whl
 $venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/*.whl
-
-# Install redundant libs in the old path
-mkdir -p $venv_old
-virtualenv --python python3 --system-site-packages --never-download $venv_old
-[ -f $deploy_dir/submodules/wheels/pip-*.whl ] && $venv_old/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/pip-*.whl
-$venv_old/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/*.whl
