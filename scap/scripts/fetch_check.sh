b6deb5d5126ba52ee0614aa1f514fcf168917d38
Adam Wight <awight@wikimedia.org>
2018-04-11 11:09:51 -0700

    Prevent virtualenv from being silly
    This flag will prevent virtualenv from discovering and attempting to download
    newer bootstrapping packages than the bundled ones.
    
    Bug: T181071
    Change-Id: I2595cc4e3678c9d4f4c67b366e8248962383d586

--- scap/scripts/fetch_check.sh
+++ scap/scripts/fetch_check.sh
@@ -12,12 +12,12 @@ git submodule update --init
 
 # Install python libs.
 mkdir -p $venv
-virtualenv --python python3 --system-site-packages $venv
+virtualenv --python python3 --system-site-packages --never-download $venv
 [ -f $deploy_dir/submodules/wheels/pip-*.whl ] && $venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/pip-*.whl
 $venv/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/*.whl
 
 # Install redundant libs in the old path
 mkdir -p $venv_old
-virtualenv --python python3 --system-site-packages $venv_old
+virtualenv --python python3 --system-site-packages --never-download $venv_old
 [ -f $deploy_dir/submodules/wheels/pip-*.whl ] && $venv_old/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/pip-*.whl
 $venv_old/bin/pip install --use-wheel --no-deps $deploy_dir/submodules/wheels/*.whl
