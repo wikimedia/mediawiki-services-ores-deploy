a51168ddab8b457dda7c9780ec898550b88f8a96
Amir Sarabadani <Ladsgroup@gmail.com>
2018-11-19 14:49:14 +0100

    Add check for celery service in scap
    Bug: T170950
    Change-Id: I0daefaf728823776f9075415e36cd73b35dcd216

--- scap/scripts/promote_check.sh
+++ scap/scripts/promote_check.sh
@@ -2,3 +2,9 @@
 
 # Restart celery.
 sudo service celery-ores-worker restart
+
+# Check if celery is up
+if [ "$(systemctl is-active celery-ores-worker.service)" = "inactive" ]; then
+  echo 'Celery is not up'
+  exit 1;
+fi
